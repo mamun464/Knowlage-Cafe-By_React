2472304b18190ccca6fb4e6edc696f502de61e80
Mamun <mamunurrashid.s.bd@gmail.com>
2023-11-14 23:10:38 +0600

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h6 className='text-6xl'>Knowlage Cafe</h6>
+    </>
+  )
+}
+
+export default App
